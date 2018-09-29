911c6c88700b74e48e9d70fe4b76b2fe52ba4e87
Kane <kmcgree14@gmail.com>
2018-09-29 11:32:12 -0500

    dsafadsf

--- server.js
+++ server.js
@@ -12,22 +12,58 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
-  });
-  
-  app.get("/tables", function(req, res) {
+});
+
+app.get("/tables", function (req, res) {
     res.sendFile(path.join(__dirname, "tables.html"));
-  });
-  
-  app.get("/reserve", function(req, res) {
+});
+
+app.get("/reserve", function (req, res) {
     res.sendFile(path.join(__dirname, "reserve.html"));
-  });
-
-  const reserved = [];
-  const waiting = [];
+});
 
 
-  app.listen(PORT, function() {
+
+const reserved = [{
+    name: "Yoda",
+    phoneNumber: 524151468684,
+    email: "ydgiahud@umn.ed",
+    UniqueID: 2000
+},
+{
+    name: "Yoda",
+    phoneNumber: 5468516416516,
+    email: "ydgiahud@umn.ed",
+    UniqueID: 2000
+},
+{
+    name: "Yoda",
+    phoneNumber: 564856516558,
+    email: "ydgiahud@umn.ed",
+    UniqueID: 2000
+}];
+
+const waiting = [];
+
+app.post("/api/characters", function (req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    var newreservation = req.body;
+
+    // Using a RegEx Pattern to remove spaces from newCharacter
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    newreservation.uniqueId = newcharacter.name.replace(/\s+/g, "").toLowerCase();
+
+    console.log(newcharacter);
+
+    characters.push(newcharacter);
+
+    res.json(newcharacter);
+});
+
+
+app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
-  });
+});
